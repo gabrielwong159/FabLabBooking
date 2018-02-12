9cc25ff62b852ea2bd37efa6d7ead3446f26b7f1
Gabriel Wong <gabrielwong15@gmail.com>
2018-02-12 09:55:19 +0800

    Added changes to support RPi, reorganized files

--- py/firebase_util.py
+++ py/firebase_util.py
@@ -1,8 +1,8 @@
 import json
 from firebase import firebase
 from functools import partial
-from selenium import webdriver
 from slot import Slot
+from webdriver_util import webdriver_book
 from telegram_util import notify_message, notify_messages
 
 with open("firebase_config.json", "r") as f:
@@ -64,16 +64,8 @@ def book(slot):
     print("Booking", slot)
     
     username, password = get_user_credentials()
-    driver = webdriver.PhantomJS()
-    driver.get(url)
-    driver.find_element_by_name("email").send_keys(username)
-    driver.find_element_by_name("password").send_keys(password)
-    driver.find_elements_by_name("login")[1].click()
-
     booking_url = slot.get_link()
-    driver.get(booking_url)
-    driver.find_element_by_class_name("create").click()
-    driver.quit()
+    webdriver_book(username, password, booking_url)
     
     notify_message("Booked: " + slot.pretty_print())
 
@@ -83,14 +75,6 @@ def notify(slots):
     messages = map(lambda slot: slot.pretty_print(), slots)
     notify_messages(messages)
 
-def load_fake_info():
-    l = []
-    l.append({"href": "reservation.php?rid=1&sid=1&rd=2017-12-19", "date": "19-12-2017", "start": "1630", "end": "1700", "autoBook": True})
-    l.append({"href": "reservation.php?rid=1&sid=1&rd=2017-12-19", "date": "19-12-2017", "start": "1530", "end": "1600", "autoBook": False})
-    firebase.put(root, "subscriptions", l)
-                       
 if __name__ == "__main__":
-    #load_fake_info()
     slots = map(Slot.dict_to_slot, firebase.get(root, "slots"))
     firebase.get_async(root, "subscriptions", partial(compare_subscriptions, slots))
-    #print(firebase.get(root, "subscriptions"))
