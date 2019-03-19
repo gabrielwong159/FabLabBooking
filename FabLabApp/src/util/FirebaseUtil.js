e526f8feef7ad57d0809184b236062b72cd5d8e5
gabrielwong159 <gabrielwong159@gmail.com>
2019-03-19 21:32:41 +0800

    Remove Firebase config from Git repo

--- FabLabApp/src/util/FirebaseUtil.js
+++ FabLabApp/src/util/FirebaseUtil.js
@@ -1,12 +1,5 @@
 import * as firebase from 'firebase';
-var firebaseConfig = {
-  apiKey: "AIzaSyBzTeYNkx9WNUEPWO0i0_yNmXMooasFrq0",
-  authDomain: "fablabbooking-3062f.firebaseapp.com",
-  databaseURL: "https://fablabbooking-3062f.firebaseio.com",
-  projectId: "fablabbooking-3062f",
-  storageBucket: "fablabbooking-3062f.appspot.com",
-  messagingSenderId: "794905869410"
-};
+import firebaseConfig from './firebase_config.json';
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 firebase.auth().signInAnonymously();
 
